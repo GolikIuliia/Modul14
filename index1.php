046fa4f12516fd7e348ccdb6e3ad822e7f8c83bf
GolikIuliia <116018335+GolikIuliia@users.noreply.github.com>
2023-01-30 14:07:18 +0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>Спа салон</title>
+        <link rel="stylesheet" href="spa.css">
+    </head>
+    <body>        
+        <header>
+            <h1>РЕЛАКС СПА САЛОН</h1>            
+        </header>
+        <main> 
+        <div class="links">
+                <a href="#">ГЛАВНАЯ</a>
+                <a href="#">УСЛУГИ</a>
+                <a href="#">О КОМПАНИИ</a>
+                <a href="#">КОНТАКТЫ</a>
+                <a href="#">РЕГИСТРАЦИЯ</a> 
+        </div>
+        <article>
+            <form action="login.php" target="_blank">
+                <!-- <form action="process.php" target="_blank"> -->
+                
+                    <button>Выйти</button>
+                <!-- </form>     -->
+            </form>
+        </article>
+        <section class="servis">          
+            <article>
+                    <a href="https://7kpacok.ru/spa-uslugi/tajskij-traditsionnyj-massazh/">                                  
+                        <h2>Уход за телом</h2>                
+                    </a>            
+                    <img src="SPA/Уход за телом.jfif">
+                    <button>Подробнее</button>
+                </article>
+            </section>    
+            <section class="servis">
+                <article>   
+                <a href="https://7kpacok.ru/spa-uslugi/tajskij-traditsionnyj-massazh/"> 
+                        <h2>Уход за лицом</h2>
+                    </a>
+                    <img src="SPA/Уход за лицом.jfif">
+                    <button>Подробнее</button>
+                </article>
+            </section>    
+            
+            </main>    
+        <footer>
+            <div class="links">
+                <a href="#">Вакансии</a>
+                <a href="#">Контакты</a>
+                <a href="#">О нас</a>
+                <a href="#">Реклама</a>
+            </div>
+            <div class="copyright">С 9:00 до 20:00, без выходныхv +7(978)000-00-00           
+            </div>
+        </footer>    
+    </body>
+
+</html> 		 
